6412c3cc0d31320d82aa788b2b6de783763277fd
Bayu Emellyo <bayuemellyo@gmail.com>
2024-11-11 15:25:24 +0700

    update navigation detail page

--- screens/Home/SingleDonationItem/SingleDonationItem.js
+++ screens/Home/SingleDonationItem/SingleDonationItem.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import style from './style';
 import {useSelector} from 'react-redux';
+import {SafeAreaView, ScrollView} from 'react-native';
+import globalstyle from '../../../assets/styles/globalStyle';
+import BackButton from '../../../components/BackButton/BackButton';
 
-const SingleDonationItem = () => {
+const SingleDonationItem = ({navigation}) => {
   const donationItemInformation = useSelector(
     state => state.donations.selectedDonationInformation,
   );
   console.log(donationItemInformation);
-  return null;
+  return (
+    <SafeAreaView style={[globalstyle.backgroundWhite, globalstyle.flex]}>
+      <ScrollView showsVerticalScrollIndicator={false} style={style.conatainer}>
+        <BackButton onPress={() => navigation.goBack()} />
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default SingleDonationItem;
